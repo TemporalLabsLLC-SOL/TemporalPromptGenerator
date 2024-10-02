3b648d2501f3bf290040d850d566777dc8361da4
TemporalLabsLLC-SOL <urbanlegendxv@gmail.com>
2024-10-01 18:22:22 -0600

    Taking Care of Business

--- WINDOWS/launch-windows.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-SET VENV_DIR=venv
-
-:: Activate the virtual environment and run the script
-call %VENV_DIR%\Scripts\activate
-echo Launching the main Python script...
-python TemporalPromptEngine.py
-
-pause
