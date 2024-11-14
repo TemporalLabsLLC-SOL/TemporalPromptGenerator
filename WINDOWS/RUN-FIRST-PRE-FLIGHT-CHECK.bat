df38513efff5d1b6472e8754c337f3fab24cafa9
Temporal Labs LLC - FOUNDER - SOL <47697347+TemporalLabsLLC-SOL@users.noreply.github.com>
2024-11-14 11:34:03 +0000

    Update RUN-FIRST-PRE-FLIGHT-CHECK.bat

--- WINDOWS/RUN-FIRST-PRE-FLIGHT-CHECK.bat
+++ WINDOWS/RUN-FIRST-PRE-FLIGHT-CHECK.bat
@@ -1,3 +1,10 @@
 @echo off
+REM Upgrade pip for Python 3.10
+py -3.10 -m pip install --upgrade pip
+
+REM Install pywin32 for Python 3.10
+py -3.10 -m pip install pywin32
+
+REM Execute the PRE-FLIGHT-CHECK.sh script using Git Bash
 "C:\Program Files\Git\bin\bash.exe" -c "./PRE-FLIGHT-CHECK.sh"
 pause
